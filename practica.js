211d9e708e850873fc34cff8a7282511dd3202b1
John Romero <jjairo5599@gmail.com>
2021-08-18 17:53:55 -0500

    Arreglo que guarda una cantidad infintina de usuarios

---
+++ practica.js
@@ -0,0 +1,34 @@
+
+var dato = new Array();
+var i = 0;
+var num = 3;
+do{
+    var mensaje = [prompt("ingresa tu nombre"),prompt("ingresa tu apellido"),prompt("ingresa tu edad")];
+
+    // i = i;
+    // num = num;
+    var j = 0;
+
+    for(i; i < num; i++){
+        j = j;
+        dato[i] =  mensaje[j];
+        j++;
+    }
+
+    respuesta = prompt("¿Deseas Ingresar mas Notas?");
+    // document.write(i +"<br>");
+    // document.write(num +"<br>");
+
+    if (respuesta == "si") {
+        i = num;
+        num = num + 3;
+    }
+
+
+}while(respuesta == "si")
+
+    for(mostrar of dato){
+        document.write(mostrar +"<br>");
+    }
+    
+
